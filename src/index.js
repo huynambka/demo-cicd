65279cb60e771f95465b7494652538bc58655d08
Nguyen Huy Nam <huynambka@gmail.com>
2025-04-21 16:41:44 +0700

    demo

--- src/index.js
+++ src/index.js
@@ -4,12 +4,16 @@ const express = require("express");
 const app = express();
 
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const cors = require("cors");
 app.use(cors());
 
 app.use("/api", require("./routes"));
+
+app.use("/", (req, res) => {
+    res.send("<h1>Welcome to the API</h1>");
+});
 
 const PORT = process.env.PORT || 8000;
 app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
